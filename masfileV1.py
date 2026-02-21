35de2081b05168641fcddd2027360bf035aa66ce
abaker244 <abaker244@wifi-10-45-17-48.wifi.berkeley.edu>
2026-02-20 22:29:23 -0800

    fixed arbitrary arguments to make my code worse because someone told me to

--- masfileV1.py
+++ masfileV1.py
@@ -3,10 +3,14 @@ import time
 import board
 import csv
 import serial
+import sys
 from adafruit_pm25.uart import PM25_UART
 
-# User Input & Logic
-timemax = int(input("How long would you like to run? "))
+arguments = sys.argv
+data_path = './' + arguments[1]
+timemax = int(arguments[2])
+file = open(data_path,'w', newline = None)
+
 timerun = 0 
 timep = 2
 
@@ -15,17 +19,17 @@ i2c = board.I2C()
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 bme680.sea_level_pressure = 1013.25
 
-# PM2.5 Setup
+# PM2.5 setup
 # (UART)
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 pm25 = PM25_UART(uart, None)
 
 print("Found PM2.5 sensor, reading data...")
 
-# Open CSV for writing *before* the loop
-with open('rundata.csv', 'w', newline='') as file:
+# Open CSV
+with file as file:
     csvwriter = csv.writer(file, delimiter=',')
-    # Optional: Add a header row so you know what the columns are
+    # header row
     csvwriter.writerow(["Timestamp", "Temp", "Gas", "Humidity", "Pressure", "PM25_Env"])
 
     while timerun < timemax:
@@ -55,10 +59,11 @@ with open('rundata.csv', 'w', newline='') as file:
             s_now = time.strftime("%A, %B %d, %Y %I:%M:%S %p", time.localtime(t_now))
             print(s_now)
 
-            # Save to CSV inside the loop
+            # Save to CSV !!inside the loop!!
             #
             csvwriter.writerow([t_now, bme680.temperature, bme680.gas, 
                                 bme680.relative_humidity, bme680.pressure, aqdata["pm25 env"]])
+            file.flush() #DO NOT REMOVE THIS IT FORCE WRITES TO PRESERVE THE DATA
 
         except RuntimeError:
             print("Unable to read from sensor, retrying...")
